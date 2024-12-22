cd255f933203acee2d35322c037de5c4324b3347
ahmedjr5050 <ahmedsabry7333@gmail.com>
2024-12-22 12:25:15 +0200

    rops update in firebase by index

--- lib/core/services/data_service.dart
+++ lib/core/services/data_service.dart
@@ -1,3 +1,8 @@
-abstract class  DatabaseService {
-  Future<void> addData({required String path, required Map<String, dynamic> data});
+abstract class DatabaseService {
+  Future<void> getdata({required String path, required Map<String, dynamic> data});
+  Future<Map<String, dynamic>?> getDataByIndex({
+    required String path,
+    required String documentId,
+    required String index,
+  });
 }
--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -3,9 +3,36 @@ import 'package:fitflow/core/services/data_service.dart';
 
 class FireStoreService implements DatabaseService {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   @override
-  Future<void> addData(
-      {required String path, required Map<String, dynamic> data}) async {
+  Future<void> getdata({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
     await firestore.collection(path).add(data);
+  }
+
+  @override
+  Future<Map<String, dynamic>?> getDataByIndex({
+    required String path,
+    required String documentId,
+    required String index,
+  }) async {
+    try {
+      final doc = await firestore.collection(path).doc(documentId).get();
+      if (doc.exists) {
+        final data = doc.data();
+        if (data != null) {
+          final details = data['details']?[index];
+          final diet = data['diet']?[index];
+          if (details != null && diet != null) {
+            return {'details': details, 'diet': diet};
+          }
+        }
+      }
+    } catch (e) {
+      print('Error fetching data: $e');
+    }
+    return null;
   }
 }
